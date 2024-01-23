44a0495bb9785f0696ff655ea9c91ba2d0897d76
Yuvraj Patkar <patkar.y@etoncollege.org.uk>
2024-01-23 10:13:25 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Patkar, Yuvraj (NA) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
