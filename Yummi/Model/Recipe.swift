492567af0620333e6bfeff39db854e6abb48ab9f
Yuvraj Patkar <patkar.y@etoncollege.org.uk>
2024-02-02 17:09:15 +0000

    some work

---
+++ Yummi/Model/Recipe.swift
@@ -0,0 +1,18 @@
+//
+//  Recipes.swift
+//  Yummi
+//
+//  Created by Patkar, Yuvraj (NA) on 02/02/2024.
+//
+
+import Foundation
+
+struct Recipe {
+    let Name: String
+    let Ingredients: [Ingredient]
+    let IsFavourite: Bool
+    let Rating: Double
+    
+}
+
+recipes = [Recipe(Name: "Grilled Cheese", Ingredients: [Ingredient(Name: <#T##String#>, Quantity: <#T##Int#>, Unit: <#T##String#>, Category: <#T##String#>, ExpiryDate: <#T##String#>, Emoji: <#T##String#>)], IsFavourite: <#T##Bool#>, Rating: <#T##Double#>)]
